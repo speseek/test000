fc770132c9abd677eef957ce39a297a88cad6de0
name <speseek@gmail.com>
2017-11-18 15:41:08 +0900

    내용을 알만한 간략한 제목
    내용은 그냥 아무거나 내가 쓰고싶은 설명 써도 됨

---
+++ src/test000/Testsum.java
@@ -0,0 +1,13 @@
+package test000;
+
+public class Testsum {
+
+	public static int a = 5;
+	public static int b = 3;
+	public static int c = a+b;
+	
+	public static void main(String[] args) {
+		System.out.println("a와 b의 합은 : " + c);
+	}
+
+}
